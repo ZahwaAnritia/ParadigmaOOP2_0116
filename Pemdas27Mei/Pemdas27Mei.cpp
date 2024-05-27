1213c4d5d5bc6f50a900b2b8ab34ff003fb3a3d6
ZahwaAnritia <anritia15@gmail.com>
2024-05-27 10:03:41 +0700

    menambahkan kelas seseorang

--- Pemdas27Mei/Pemdas27Mei.cpp
+++ Pemdas27Mei/Pemdas27Mei.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 using namespace std;
 
+class seseorang {
+public:
+    virtual void pesan() = 0;
+    /* virtual void pesan(){
+    cout<<*pesan dari seseorang*<<endl;
+    } */
+};
 
-int main()
-{
-    
+int main () {
+
 }
