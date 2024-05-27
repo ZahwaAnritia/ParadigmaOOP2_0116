244dabb1c47cb1230d8d946a9ccb52c1eadb3a57
ZahwaAnritia <anritia15@gmail.com>
2024-05-27 10:10:30 +0700

    menambahkan pointer menu utama

--- Pemdas27Mei/Pemdas27Mei.cpp
+++ Pemdas27Mei/Pemdas27Mei.cpp
@@ -18,10 +18,21 @@ public:
 
 class lia :public seseorang {
 public:
-    void pesam() {
+    void pesan() {
         cout << "Pesan dari lia" << endl;
     }
 };
 int main () {
+    seseorang* obyek;
+    joko a;
+    lia b;
+
+    obyek = &a;
+    obyek->pesan();
+    obyek = &b;
+    obyek->pesan();
+    //a.seseorang::pesan();
+
+    return 0;
 
 }
